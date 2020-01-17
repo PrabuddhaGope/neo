283efff60520fe56393e00e0a061c374640f1106
JituR101 <jitendra98rahangdale@gmail.com>
2020-01-17 23:24:09 +0530

    register form added

--- reglog/dbconnect.php
+++ reglog/dbconnect.php
@@ -1,8 +1,9 @@
 <?php
   $db_host = "localhost";
-  $db_username = "root";
+  $db_username = "neouser";
   $db_name = "neo";
+  $db_password = "Neo@123";
 
-  $con = mysqli_connect("$db_host","$db_username") or die ("could not connect to mysql");
+  $con = mysqli_connect("$db_host","$db_username", "$db_password") or die ("could not connect to mysql");
   mysqli_select_db($con,$db_name) or die ("no database");
 ?>
--- reglog/register.php
+++ reglog/register.php
@@ -1,6 +1,6 @@
 <?php
-  error_reporting(E_ALL);
-  ini_set('display_errors', '1');
+  //error_reporting(E_ALL);
+  //ini_set('display_errors', '1');
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
   require '../vendor/autoload.php';
@@ -142,18 +142,6 @@
               </body>
           </html>';
 
-      // $url = 'https://startupconclave.ecellvnit.org/send';
-      // $data = array('subject' => $subject, 'email' => $to, 'html' => $html, 'pass' => 'intheend');
-      // use key 'http' even if you send the request to https://...
-      // $options = array(
-      //   'http' => array(
-      //       'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-      //       'method'  => 'POST',
-      //       'content' => http_build_query($data)
-      //   )
-      // );
-      // $context  = stream_context_create($options);
-      // $result = file_get_contents($url, false, $context);
       if(!$mail->send()){
         echo 'Message was not send';
       }
@@ -167,5 +155,3 @@
     header("location:../index.php");
   }
   ?>
-
-  
